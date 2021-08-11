a2d905444737992a9821d9a22f8d96f1df314e29
ykn9080 <ykn9080@empal.com>
2021-08-11 10:53:07 +0900

    success npm packaging

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import AntFormDisplay from "./Form/AntFormDisplay";
 
+
 ReactDOM.render(
   <>
-    <AntFormDisplay />
+    <AntFormDisplay formid="5ea0e1a12c3b2808b4da0826"/>
   </>,
   document.getElementById("root")
 );
